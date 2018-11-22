ce691c9543f97e12cc748641fffc9b3ec336e725
Harshit Pareek <hp1014@nyu.edu>
2018-11-21 20:59:51 -0800

    feat(first commit)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as jspdf from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  ngOnInit() {
+    const sourceElement = document.getElementById("pdfContainer");
+    html2canvas(sourceElement).then((canvas) => {
+      console.log("the value of canvas is: ", canvas);
+    });
+  }
+
 }
