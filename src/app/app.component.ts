71e69a0e792bb4a4572c5100626142242b937276
Harshit Pareek <hp1014@nyu.edu>
2019-09-26 20:12:37 -0700

    feat(cleaning the slate)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as jspdf from "jspdf";
-import html2canvas from "html2canvas";
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
+
   ngOnInit() {
-    const sourceElement = document.getElementById("pdfContainer");
-    html2canvas(sourceElement).then((canvas) => {
-      console.log("the value of canvas is: ", canvas);
-    });
   }
 
 }
